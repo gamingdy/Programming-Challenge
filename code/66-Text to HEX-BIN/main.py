6ea15c3ec94cc78f08adb58e9441b3f7a589a998
Gamingdy <christgik12@gmail.com>
2021-12-28 23:55:00 +0100

    start text/hex/bin converter

---
+++ code/66-Text to HEX-BIN/main.py
@@ -0,0 +1,63 @@
+# author : gamingdy
+
+import sys
+
+
+class Converter:
+    def text_to_hex(user_string):
+        return user_string.encode("utf-8").hex()
+
+    def text_to_bin(user_string):
+        bin_text = ""
+        for char in user_string:
+            bin_number = bin(ord(char))[2:]
+            if len(bin_number) < 8:
+                bin_number = "0" * (8 - len(bin_number)) + bin_number
+
+            bin_text += bin_number + " "
+        return bin_text
+
+    def hex_to_text(user_string):
+        pass
+
+    def hex_to_bin(user_string):
+        pass
+
+    def bin_to_text(user_string):
+        pass
+
+    def bin_to_hex(user_string):
+        pass
+
+
+all_option = {
+    "1": ("Convert text to hex", Converter.text_to_hex),
+    "2": ("Convert text to bin", Converter.text_to_bin),
+    "3": ("Convert hex to text", Converter.hex_to_text),
+    "4": ("Convert hex to bin", Converter.hex_to_bin),
+    "5": ("Convert bin to text", Converter.bin_to_text),
+    "6": ("Convert bin to hex", Converter.bin_to_text),
+    "0": ("Exit",),
+}
+
+
+def print_option():
+    sep = 1
+    for i in all_option:
+        print(f"[{i}] : {all_option[i][0]}")
+        sep += 1
+        if sep > 2:
+            print("-" * 20)
+            sep = 1
+    print()
+
+
+while True:
+    print_option()
+    user_choice = input("Choose an option: ")
+    if user_choice in all_option:
+        if user_choice == "0":
+            sys.exit()
+        user_string = input(f"String to {all_option[user_choice][0].lower()}: ")
+        result = all_option[user_choice][1](user_string)
+        print("This your result", result)
