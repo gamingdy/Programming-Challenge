7540257dd58cbe132781f5ce9afcfc02cf697e94
Gamingdy <christgik12@gmail.com>
2021-12-31 17:52:33 +0100

    update char occurence with std::count

--- code/complete/46-Char occurence in String/main.cpp
+++ code/complete/46-Char occurence in String/main.cpp
@@ -1,6 +1,7 @@
 // author : gamingdy
 
 #include <iostream>
+#include <algorithm>
 
 int main(){
 	std::string user_string {"This is an string exemple"}; // Put your string here
@@ -8,12 +9,7 @@ int main(){
 	char user_char {'a'}; // Put the char you want to count the occurence in the given string
 
 
-	int occurence_char {};
-	for (char element : user_string){
-		if (element == user_char){
-			occurence_char ++;
-		}
-	}
+	auto occurence_char {std::count(std::begin(user_string), std::end(user_string), user_char)};
 
 	std::cout << "There are " << occurence_char << " "<<user_char << " in given string: " << user_string;
 
