7acae46e58d46729135de5c4061b6fdc8583b40e
Gamingdy <christgik12@gmail.com>
2023-05-27 02:40:20 +0200

    feat: reverse CHARACTER list

--- code/18 - Image to ASCII Art/main.py
+++ code/18 - Image to ASCII Art/main.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 
 
-CHARACTER = ["@", "#", "$", "%", "?", "*", "+", ";", ":", ",", "."]
+CHARACTER = [".", ",", ":", ";", "+", "*", "?", "%", "$", "#", "@"]
 
 
 
